d3e396771cd1438995eb1559f82d86e6478a5736
Ali-Sa3d-G <as7476980@gmail.com>
2026-05-02 23:18:38 +0300

    New Features

--- smart_wallet/transactions/urls.py
+++ smart_wallet/transactions/urls.py
@@ -1,15 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
+from .reports_view import ReportsView
 
 
 router = DefaultRouter()
-router.register('transactions', views.TransactionsViewSets,basename='transactions')
-router.register('categories',views.CategoryViewSets,basename='categories')
-router.register('budgets',views.BudgetViewSets,basename='budgets')
+router.register('transactions', views.TransactionsViewSets, basename='transactions')
+router.register('categories',   views.CategoryViewSets,    basename='categories')
+router.register('budgets',      views.BudgetViewSets,       basename='budgets')
 
-urlpatterns=[
-   
+urlpatterns = [
+    path('reports/', ReportsView.as_view(), name='reports'),
 ]
 
 urlpatterns += router.urls
